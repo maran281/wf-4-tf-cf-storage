52f7c619b1afaa0dc7f44951ff466832d380e1c6
mukesh sain <mukeshk.sain786@gmail.com>
2023-11-02 22:01:49 +0100

    cm1

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,11 @@ resource "google_storage_bucket" "tf_storage_bucket" {
 resource "google_storage_bucket_object" "tf_bucket_object" {
   name = "index.zip"
   bucket = google_storage_bucket.tf_storage_bucket.name
-  source = "mycode.zip"
+  source = "myapp.zip"
 }
 
-resource "google_cloudfunctions_function" "tf_cloud_unct" {
-    name = "my_first_cf"
+resource "google_cloudfunctions_function" "tf_cloud_funct" {
+    name = "hello_world"
     runtime = "Python"
     source_archive_bucket = google_storage_bucket.tf_storage_bucket.name
     source_archive_object = google_storage_bucket_object.tf_bucket_object.name
