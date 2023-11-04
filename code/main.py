48353a1968106baee7b31c64bf1a5c5ff3a9f348
mukesh sain <mukeshk.sain786@gmail.com>
2023-11-04 23:07:46 +0100

    2

--- code/main.py
+++ code/main.py
@@ -1,16 +1,15 @@
+import os
+from google.cloud import storage
+
 def hello_world(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+    bucket_name = "source_bucket_for_cf_1"
+    file_name = "test.json"
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blob = bucket.blob(file_name)
+
+    json_data = blob.download_as_text()
+
+    return json_data
