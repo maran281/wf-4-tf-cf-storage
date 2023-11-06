c9bfe81014c138a9fc5b8734d9dfdaf8ccada40b
mukesh sain <mukeshk.sain786@gmail.com>
2023-11-06 22:16:28 +0100

    commit new

--- code/main.py
+++ code/main.py
@@ -1,6 +1,23 @@
-import functions_framework
+from google.cloud import storage
 
 @functions_framework.http
 
 def hello_world(request):
-    return 'hi Manoj'
+    # Initialize a Cloud Storage client
+    storage_client = storage.Client()
+
+    # Get a reference to the source blob (file) you want to rename
+    source_bucket = storage_client.get_bucket('source_file_bucket_m')
+    source_blob = source_bucket.blob('test.json')
+
+    # Copy the source blob to the destination with a new name
+    destination_bucket = storage_client.get_bucket(source_bucket)
+    destination_blob = source_bucket.copy_blob(source_blob, destination_bucket, 'test_completed.json')
+
+    # Delete the original source blob (optional)
+    source_blob.delete()
+
+    print(f"File '{test.json}' has been renamed to '{test_completed.json}' in bucket '{source_file_bucket_m}'.")
+
+
+    return 'Task completed'
